def5ae381da2c0e571c687a2cdbc68e8bdf5985f
Ramu <naciri.ilyes@live.fr>
2026-04-06 22:30:18 +0200

    fix: flake8 a_maze_ing.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -20,16 +20,18 @@ def main() -> None:
             perfect = config["PERFECT"]
             start_x, start_y = entry
             seed = config.get("SEED")
-            mg = MazeGenerator(width, height, start_x, start_y,perfect, seed)
+            mg = MazeGenerator(width, height, start_x, start_y, perfect, seed)
             mg.generate()
             grid = mg.grid
             mg.generate()
             grid = mg.grid
             if tuple(entry) in mg.pattern_cell:
-                print("Warning: entry lands on '42' pattern, defaulting to (0,0)")
+                print("Warning: entry lands on '42' pattern, "
+                      "defaulting to (0,0)")
                 entry = [0, 0]
             if tuple(exit_point) in mg.pattern_cell:
-                print("Warning: exit lands on '42' pattern, defaulting to (width-1, height-1)")
+                print("Warning: exit lands on '42' pattern, "
+                      "defaulting to (width-1, height-1)")
                 exit_point = [width-1, height-1]
             path = solve(grid, entry, exit_point)
             path = solve(grid, entry, exit_point)
